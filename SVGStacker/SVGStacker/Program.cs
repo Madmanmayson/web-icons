36151b5e08882316bd9da146b040112a6c3708ef
Unknown <madmanmayson45@gmail.com>
2021-01-25 20:53:26 -0800

    Add C# Project for SVGStacker

---
+++ SVGStacker/SVGStacker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SVGStacker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
