19f2abc598aac3f4f9526a090ff9de8e05a776ac
Unknown <madmanmayson45@gmail.com>
2021-01-26 17:58:44 -0800

    Added main XML generation code
    Note: not a perfect solution but a working one assuming everything is paths with fills (no internal styles) so will likely be revised in the future

--- SVGStacker/SVGStacker/SVGStacker.cs
+++ SVGStacker/SVGStacker/SVGStacker.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Xml;
 
 namespace SVGStacker
 {
@@ -11,7 +12,7 @@ namespace SVGStacker
             try
             {
                 String path;
-                if (args.Length == 0)
+                if (args.Length == 0) //Checking if folder was dragged onto exe or path provided as args
                 {
                     Console.WriteLine("Please input the full path to the icon folder");
                     path = Console.ReadLine();
@@ -23,7 +24,7 @@ namespace SVGStacker
                 if (!Directory.Exists(path))
                 {
                     Console.WriteLine("ERROR: {0} is not a valid directory.", path);
-                    throw new DirectoryNotFoundException(); //Will break out of try
+                    throw new DirectoryNotFoundException("Invalid directory provided"); //Will break out of try
                 } 
 
                 string[] files = Directory.GetFiles(path);
@@ -36,14 +37,16 @@ namespace SVGStacker
                 else
                 {
                     Console.WriteLine("ERROR: {0} does not contain any SVG files", path);
+                    throw new FileNotFoundException("Directory did not contain .svg files"); //Will break out of try
                 }
-                
             }
             catch (Exception e)
             {
                 File.WriteAllText("log_" + DateTime.Now.ToString("yyyyMMddTHHmmss"), e.ToString());
+                
             }
-
+            Console.WriteLine("Press any key to exit");
+            Console.Read();
         }
 
         private static List<String> getSVGFiles (String[] files)
@@ -51,7 +54,7 @@ namespace SVGStacker
             List<String> svgs = new List<String>();
             foreach(String file in files)
             {
-                if(Path.GetExtension(file).ToLower() != ".svg")
+                if(Path.GetExtension(file).ToLower() == ".svg")
                 {
                     svgs.Add(file);
                 }
@@ -61,7 +64,75 @@ namespace SVGStacker
 
         private static void buildFile(List<String> svgs)
         {
+            //Creating the XmlWriter w/out the xml declaration
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.OmitXmlDeclaration = true;
+            XmlWriter writer = XmlWriter.Create("symbol-defs.svg", settings);
 
+            //Creating the base SVG declaration with appropriate attributes. Includes <svg> and <defs>
+            writer.WriteStartElement("svg", "http://www.w3.org/2000/svg");
+            writer.WriteAttributeString("style", "display: none");
+            writer.WriteAttributeString("aria-hidden", "true");
+            writer.WriteStartElement("defs");
+
+            //Looping through the list of SVG files
+            int progress = 0;
+            foreach (String file in svgs)
+            {
+                using (var reader = XmlReader.Create(file))
+                {
+                    //Building <symbol> tag & attributing it
+                    reader.Read();
+                    reader.MoveToAttribute("viewBox");
+                    writer.WriteStartElement("symbol");
+                    writer.WriteAttributeString("id", Path.GetFileNameWithoutExtension(file));
+                    writer.WriteAttributeString("viewBox", reader.ReadContentAsString());
+
+                    
+
+                    //Getting the path data and copying it inside the symbol tag
+                    reader.Read();
+                    if (reader.Name != "g"){ //depending on the file, there might be a <g> tag we want to avoid
+                        XmlReader inner = reader.ReadSubtree();
+
+                        //duplicating all path elements by creating new nodes in the output
+                        while (inner.Read())
+                        {
+                            writer.WriteStartElement(inner.Name); 
+                            while (inner.MoveToNextAttribute())
+                            {
+                                writer.WriteAttributeString(inner.Name, inner.Value);
+                            }
+                            writer.WriteEndElement();
+                        }
+
+                    } 
+                    else
+                    {
+                        //duplicating all path elements by creating new nodes in the output
+                        while (reader.Read() && reader.Name != "g") //checking for closing </g> tag
+                        {
+                            writer.WriteStartElement(reader.Name);
+                            while (reader.MoveToNextAttribute())
+                            {
+                                writer.WriteAttributeString(reader.Name, reader.Value);
+                            }
+                            writer.WriteEndElement();
+                        }
+                    }
+
+                    //Closing </symbol>
+                    writer.WriteEndElement();
+
+                    progress++;
+                    Console.WriteLine("{0} processed. {1}/{2} completed.", Path.GetFileName(file), progress, svgs.Count);
+                }
+            }
+
+            //Closing the </svg> and </defs> tags and the XmlWriter allowing it to save the file.
+            writer.WriteEndElement();
+            writer.WriteEndElement();
+            writer.Close();
         }
     }
 }
